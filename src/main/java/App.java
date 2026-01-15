b2061fe473f3eb5dc628cf9041f81147b2eecc95
dishabirje2003 <154901726+dishabirje2003@users.noreply.github.com>
2026-01-15 17:17:08 +0530

    Add success message to main method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello CI/CD World!");
+        System.out.println("Ci moven built successfully");
     }
 }
+
